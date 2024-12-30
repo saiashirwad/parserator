3521c810c2fd8d16241117f93386cdcf5a6e9c77
Sai Ashirwad <saiashirwadr@gmail.com>
2024-12-30 08:59:45 +0530

    wip

---
+++ src/maybe.ts
@@ -0,0 +1,85 @@
+export type Maybe<A> = None | Some<A>
+
+export class None {
+	readonly _tag = "None"
+}
+
+export class Some<A> {
+	readonly _tag = "Some"
+	constructor(public readonly value: A) {}
+}
+
+export class GenMaybe<A> {
+	constructor(readonly op: Maybe<A>) {}
+
+	*[Symbol.iterator](): Generator<GenMaybe<A>, A, any> {
+		return yield this
+	}
+}
+
+export const Maybe = {
+	none<A = never>(): Maybe<A> {
+		return new None()
+	},
+
+	some<A>(value: A) {
+		return new Some(value)
+	},
+
+	isNone<A>(maybe: Maybe<A>): maybe is None {
+		return maybe._tag === "None"
+	},
+
+	isSome<A>(maybe: Maybe<A>): maybe is Some<A> {
+		return maybe._tag === "Some"
+	},
+
+	match<A, B>(
+		maybe: Maybe<A>,
+		patterns: {
+			onNone: () => B
+			onSome: (value: A) => B
+		},
+	): B {
+		if (Maybe.isNone(maybe)) {
+			return patterns.onNone()
+		}
+		return patterns.onSome(maybe.value)
+	},
+
+	of<A>(value: Maybe<A>): GenMaybe<A> {
+		return new GenMaybe(value)
+	},
+
+	gen<T>(
+		f: (
+			adapter: <A>(m: Maybe<A>) => GenMaybe<A>,
+		) => Generator<GenMaybe<any>, T, any>,
+	): Maybe<T> {
+		const adapter = <A>(m: Maybe<A>) => new GenMaybe(m)
+		const iterator = f(adapter)
+		let current = iterator.next()
+
+		while (!current.done) {
+			const maybe = current.value.op
+			if (Maybe.isNone(maybe)) {
+				return maybe
+			}
+			current = iterator.next(maybe.value)
+		}
+
+		return Maybe.some(current.value)
+	},
+}
+
+// const hi: Maybe<number> = Maybe.none()
+// const there: Maybe<number> = Maybe.some(5)
+
+// const lol = Maybe.gen(function* ($) {
+// 	const a = yield* $(there)
+// 	const b = yield* $(hi)
+
+// 	return a + b
+// })
+
+// console.log(lol)
