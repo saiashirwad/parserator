f5ddea363b4e3e2aa43511d7d9e7dcaee707d1ad
Sai Ashirwad <saiashirwadr@gmail.com>
2025-01-04 09:01:51 +0530

    wip

--- src/utils.ts
+++ src/utils.ts
@@ -1,23 +1,25 @@
-import { Parser } from "./parser";
+import { Parser } from "./parser"
 
 export const peekState = new Parser((s) => {
-  console.log(s);
-  return Parser.succeed(undefined, s);
-});
+	console.log(s)
+	return Parser.succeed(undefined, s)
+})
 
 export const peekRemaining = new Parser((s) => {
-  console.log(s.remaining);
-  return Parser.succeed(s.remaining, s);
-});
+	console.log(s.remaining)
+	return Parser.succeed(s.remaining, s)
+})
 
 export const peekAhead = (n: number) =>
-  new Parser((s) => {
-    return Parser.succeed(s.remaining.slice(0, n), s);
-  });
-
+	new Parser((s) => {
+		return Parser.succeed(s.remaining.slice(0, n), s)
+	})
 
 export const peekLine = new Parser((s) => {
-  const restOfLine = s.remaining.slice(0, s.remaining.indexOf('\n'))
-  console.log(restOfLine)
-  return Parser.succeed(restOfLine, s);
-});
+	const restOfLine = s.remaining.slice(
+		0,
+		s.remaining.indexOf("\n"),
+	)
+	console.log(restOfLine)
+	return Parser.succeed(restOfLine, s)
+})
