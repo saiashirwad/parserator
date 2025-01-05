bcd68801ba8ae065e24e0b7ce05d10e96af7b598
Sai Ashirwad <saiashirwadr@gmail.com>
2025-01-05 14:28:42 +0530

    wip

--- tests/lol.ts
+++ tests/lol.ts
@@ -1,9 +1,15 @@
-import { ParserError, char, string } from "../src"
+import { Parser, ParserError, string } from "../src"
 
-const lol = string("abc")
-const result = lol.parseOrError(".abc")
+const lol = Parser.gen(function* () {
+	yield* string("abc")
+	return yield* string("def")
+})
+
+const result = lol.parseOrError("abcdef")
 
 if (result instanceof ParserError) {
 	console.log(result.pos)
 	console.error(result.message)
+} else {
+	console.log(result)
 }
