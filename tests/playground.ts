b533639bf6d815fc749cad0b5cb19790eb952d55
Sai Ashirwad <saiashirwadr@gmail.com>
2025-01-29 20:35:50 +0530

    wip

--- tests/playground.ts
+++ tests/playground.ts
@@ -4,31 +4,33 @@ import {
 	State,
 	char,
 	many0,
+	many1,
 	optional,
 	or,
 	parser,
 } from "../src"
 
-const whitespace = or(char(" "), char("\n"))
+const requiredWhitespace = many1(or(char(" "), char("\n"))).withError(
+	() => "Expected a whitespace here",
+)
 
 const something = parser(function* () {
 	yield* char("h")
-	yield* whitespace.withError(() => "hi")
-	yield* whitespace.withError(() => "hi")
-	yield* whitespace.withError(() => "hi")
+	yield* requiredWhitespace
 	const something = yield* optional(char("."))
 	if (something) {
 		const message = "I did not expect a '.' there :("
-		return yield* Parser.error(message, [], (state) => State.move(state, -1))
+		return yield* Parser.error(message, [""], (state) => State.move(state, -1))
 	}
 	yield* char("i")
-	yield* whitespace
+	yield* requiredWhitespace
 
 	return { hi: true }
 })
 
-const result = something.parseOrError("h\n\n\n.hasdf")
+const result = something.parseOrError("h\n\n.hasdf")
 if (result instanceof ParserError) {
+	console.log(result)
 	console.error(result.message)
 } else {
 }
