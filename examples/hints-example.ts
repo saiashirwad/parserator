362c23a9c1465630f7802726606af850aab96903
Sai Ashirwad <saiashirwadr@gmail.com>
2025-06-05 16:18:04 +0530

    hints example

---
+++ examples/hints-example.ts
@@ -0,0 +1,11 @@
+import { anyKeywordWithHints, char, many1 } from "../src";
+
+const lol = many1(char("$")).zip(anyKeywordWithHints(["name", "hi", "typescript"]));
+
+const inputs = ["$ha", "$$$$nome"];
+for (const input of inputs) {
+  const result = lol.parse(input);
+  console.log(
+    result.result._tag === "Left" ? result.result.left.format("ansi") : result.result.right
+  );
+}
